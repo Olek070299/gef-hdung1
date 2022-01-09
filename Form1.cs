d2ccf0c8bab6b28a377434c1ce9f3c72b6f0afa6
Olek070299 <82806272+Olek070299@users.noreply.github.com>
2022-01-09 17:13:00 +0100

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -24,7 +24,8 @@ namespace Gefärdungberechnen
 
         private void button1_Click(object sender, EventArgs e)
         {
-            test.Gesamtleistung = Convert.ToDouble(textBox1.Text);
+            test.Gesamtleistungkamera = double.Parse(textBox1.Text);
+             test.Gesamtleistung14rad = double.Parse(textBox6.Text);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -39,12 +40,7 @@ namespace Gefärdungberechnen
 
         }
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-            test.Flag= Convert.ToBoolean(textBox2.Text);
-
-            test.spektrumnomrieren();
-        }
+       
 
         private void button5_Click(object sender, EventArgs e)
         {
@@ -54,23 +50,109 @@ namespace Gefärdungberechnen
         private void button6_Click(object sender, EventArgs e)
         {
             test.readdata();
+
+
+            pictureBox1.Image = test.Bitma;
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            test.getGefährdungsstufe();
+          
+
+
+         
+           
+            test.getBlaulicht();
+           
+            test.getNetzhautthermisch();
+            test.getNetzhautthermischschwacherreiz();
+
+            test.getaktinischesUVHautAuge();
+            test.getAugeUVA();
+            test.getIRSTRahlung();
+
+
+
+
 
             pictureBox1.Image = test.Bitma;
            
-                listBox1.Items.Add(test.ExpositionsgrenzwertA);
+            listBox1.Items.Add("EwertA="+test.ExpositionsgrenzwertA);
+            listBox1.Items.Add("EwertB=" + test.ExpositionsgrenzwertB);
+            listBox1.Items.Add("EwertC=" + test.ExpositionsgrenzwertC);
+            listBox1.Items.Add("EwertD=" + test.ExpositionsgrenzwertD);
+            listBox1.Items.Add("EwertE=" + test.ExpositionsgrenzwertE);
+            listBox1.Items.Add("EwertF=" + test.ExpositionsgrenzwertF);
+            listBox1.Items.Add("EwertG=" + test.ExpositionsgrenzwertG);
+            listBox1.Items.Add("EwertH=" + test.ExpositionsgrenzwertH);
+            listBox1.Items.Add("EwertI=" + test.ExpositionsgrenzwertI);
+            listBox1.Items.Add("EwertJ=" + test.ExpositionsgrenzwertJ);
+            listBox1.Items.Add("EwertK=" + test.ExpositionsgrenzwertK);
+            listBox1.Items.Add("EwertL=" + test.ExpositionsgrenzwertL);
+            listBox1.Items.Add("EwertM=" + test.ExpositionsgrenzwertM);
+            listBox1.Items.Add("EwertN=" + test.ExpositionsgrenzwertN);
+            listBox1.Items.Add("EwertO=" + test.ExpositionsgrenzwertO);
+
+
+
+            listBox1.Items.Add("EwertA"+test.ExpositionswertA);
+            listBox1.Items.Add("Ewertb" + test.ExpositionswertB);
+            listBox1.Items.Add("Ewertc" + test.ExpositionswertC);
+            listBox1.Items.Add("Ewertd" + test.ExpositionswertD);
+            listBox1.Items.Add("Ewerte" + test.ExpositionswertE);
+            listBox1.Items.Add("Ewertf" + test.ExpositionswertF);
+            listBox1.Items.Add("Ewertg" + test.ExpositionswertG);
+            listBox1.Items.Add("Ewerth" + test.ExpositionswertH);
+            listBox1.Items.Add("Ewerti" + test.ExpositionswertI);
+            listBox1.Items.Add("Ewertj" + test.ExpositionswertJ);
+            listBox1.Items.Add("Ewertk" + test.ExpositionswertK);
+            listBox1.Items.Add("Ewertl" + test.ExpositionswertL);
+            listBox1.Items.Add("Ewertm" + test.ExpositionswertM);
+            listBox1.Items.Add("Ewertn" + test.ExpositionswertN);
+            listBox1.Items.Add("Ewerto" + test.ExpositionswertO);
+
+           
+
 
 
         }
         
+         private void button9_Click(object sender, EventArgs e)
+        {
+            test.Empfangswinkel = double.Parse(textBox4.Text);
+            test.Bildabstand = double.Parse(textBox5.Text);
 
+
+            test.gethotspot(test.Empfangswinkel, test.Bildabstand);
+            
+            test.getAugeHautUVlichtgefahrTROS();
+            test.getkataraktogeneseTROS();
+            test.getHornhautverbrennungundkataraktogeneseTROS();
+            test.gethautverbrennungTROS();
+            
+
+            test.getblaulichtgefahrAugeTROS(double.Parse(textBox4.Text));
+
+            test.getVerbrennungsgefahrschwachervisuellerreizTROS(double.Parse(textBox4.Text));
+            test.getVerbrennungsgefahrTROS(double.Parse(textBox4.Text));
+
+           pictureBox1.Image =test.Bitma;
+
+
+
+
+        }
         private void button3_Click(object sender, EventArgs e)
         {
             test.testbitmap();
         }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            test.Time = double.Parse(textBox3.Text);
+            
+        }
+
+      
     }
 }
